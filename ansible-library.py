efbcb7d98beb22688ebce060b5119c0e596911c8
Javier Palacios <javiplx@gmail.com>
2016-02-20 17:10:54 +0100

    Recover executable script

---
+++ ansible-library.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Copyright (C) 2016 Javier Palacios
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# Version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See full
+# GPLv2 for more details (http://www.gnu.org/licenses/gpl-2.0.html).
+
+import ansible_library
+
+if __name__ == "__main__":
+    ansible_library.app.roles_dir = '/var/lib/galaxy'
+    ansible_library.app.run(host="0.0.0.0", port=3333, debug=True)
+
+
