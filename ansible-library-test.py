e4177223d755cde02f9c0a01fd044dbdbe2dc8b6
Javier Palacios <javiplx@gmail.com>
2016-02-20 17:17:47 +0100

    Adapt tests for module

--- ansible-library-test.py
+++ ansible-library-test.py
@@ -1,5 +1,17 @@
+#!/usr/bin/python
 
-import XXX
+# Copyright (C) 2016 Javier Palacios
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# Version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See full
+# GPLv2 for more details (http://www.gnu.org/licenses/gpl-2.0.html).
+
+import ansible_library
 import unittest
 import json
 
@@ -7,8 +19,9 @@ import json
 class ansible_library_test ( unittest.TestCase ) :
 
     def setUp ( self ) :
-        XXX.app.roles = XXX.read_roles( '/var/lib/galaxy' )
-        self.app = XXX.app.test_client()
+        ansible_library.app.roles_dir = '/var/lib/galaxy'
+        ansible_library.app.load_roles()
+        self.app = ansible_library.app.test_client()
 
     def get ( self , url ) :
         res = self.app.get( "/api/%s" % url )
