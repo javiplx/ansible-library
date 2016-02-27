cbddeb1ff7ca8f3680105e6d663ad0160fbe8aa4
Javier Palacios <javier.palacios@feverup.com>
2016-02-27 11:28:36 +0000

    Add missing requirements

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 
 setup(
     name = 'ansible-library',
-    version = '0.9',
+    version = '0.9.1',
     description = 'Minimal galaxy server to host private roles',
     author = 'Javier Palacios',
     author_email = 'javiplx@gmail.com',
     license = 'GPLv2',
     url = 'https://github.com/javiplx/ansible-library',
     scripts = [ 'ansible-library.py' ],
-    install_requires=[ 'Werkzeug>=0.9' , 'flask>=0.10' ]
+    install_requires=[ 'Werkzeug>=0.9' , 'flask>=0.10' , 'ansible>=1.9.2' , 'pyyaml' ]
     )
 
